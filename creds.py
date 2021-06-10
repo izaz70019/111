f14c8be15f1a06a456c2adb8b29d16c627d3188f
izazkhan1 <83175052+izazkhan1@users.noreply.github.com>
2021-06-11 00:49:26 +0500

    Update creds.py

--- creds.py
+++ creds.py
@@ -18,4 +18,4 @@ class Creds():
     #Example 
     #TG_TOKEN = "dkjfksdkffdkfdkfdj"
     #TEAMDRIVE_FOLDER_ID = "13v4MaZnBz-iEHlZ0gFXk7rh"
-    #TEAMDRIVE_ID = "0APh6R4WVvguEUk9PV"
+    #TEAMDRIVE_ID = "0APh6R4WVvvguEUk9PV"
